ba8981ac8a1e41bd714ec23913cdd5f6b6056e9e
Vinicius Monteiro Orlandi <vinicius.orlandi@pampa.compasso>
2021-08-03 16:08:43 -0300

    res 200

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ const port = process.env.PORT
 const app = express()
 app.use(express.json())
 
-app.get('/sobre', (req, res) => { return res.status(400).json({ name: 'Vinicius Monteiro Orlandi', age: 23, course: "Ciência Da Computação", email: "dev.viniciusmonteiro@gmail.com" }) })
+app.get('/sobre', (req, res) => { return res.status(200).json({ name: 'Vinicius Monteiro Orlandi', age: 23, course: "Ciência Da Computação", email: "dev.viniciusmonteiro@gmail.com" }) })
 
-app.get('/social', (req, res) => { return res.status(400).json({ Facebook: 'https://www.facebook.com/vinicius.monteiro.7549/', linkedin: "https://www.linkedin.com/in/vinicius-monteiro-orlandi-8b06191b4/", github: "https://github.com/dev-ViniciusMonteiro" }) })
+app.get('/social', (req, res) => { return res.status(200).json({ Facebook: 'https://www.facebook.com/vinicius.monteiro.7549/', linkedin: "https://www.linkedin.com/in/vinicius-monteiro-orlandi-8b06191b4/", github: "https://github.com/dev-ViniciusMonteiro" }) })
 
-app.get('/image/perfil', (req, res) => res.status(400).json({ image: "https://media.discordapp.net/attachments/796490440429404192/872175099636240435/eu.jpg?width=594&height=594" }))
+app.get('/image/perfil', (req, res) => res.status(200).json({ image: "https://media.discordapp.net/attachments/796490440429404192/872175099636240435/eu.jpg?width=594&height=594" }))
 
-app.get('/', (req, res) => res.json({ image: "Ola Mundo" }))
-
-
+app.get('/', (req, res) => res.json({ msg: "Ola Mundo" }))
 
 app.listen(port, (req, res) => { console.log("API rodando na porta:" + port) })

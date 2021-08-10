eba0aa3c389a8f141b1b7be40514339da0ed5ea5
Vinicius Monteiro Orlandi <vinicius.orlandi@pampa.compasso>
2021-08-10 10:26:17 -0300

    Buscador cep

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 const express = require('express')
-
+var cors = require('cors')
 const port = process.env.PORT
 
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.get('/sobre', (req, res) => { return res.status(200).json({ name: 'Vinicius Monteiro Orlandi', age: 23, course: "Ciência Da Computação", email: "dev.viniciusmonteiro@gmail.com" }) })
 
@@ -13,6 +14,14 @@ app.get('/social', (req, res) => { return res.status(200).json({ Facebook: 'http
 
 app.get('/image/perfil', (req, res) => res.status(200).json({ image: "https://media.discordapp.net/attachments/796490440429404192/872175099636240435/eu.jpg?width=594&height=594" }))
 
+app.get('/cep/generator', (req, res) => res.status(200).json({ CEP: `1${getRandomInt(1000, 9999)}120` }))
+
 app.get('/', (req, res) => res.json({ msg: "Ola Mundo" }))
 
 app.listen(port, (req, res) => { console.log("API rodando na porta:" + port) })
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
